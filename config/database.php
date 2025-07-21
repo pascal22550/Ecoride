2447498e51c713bc3f434a68a01e1e1afb35cda3
Pascal <madinfopc@gmail.com>
2025-07-21 22:48:43 +0200

    mise à jour connexion à la base de données pour pouvoir déployer le site

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 function connectDB() {
     $host = 'localhost';
-    $dbname = 'ecoride';
-    $username = 'root';
-    $password = 'root';
+    $dbname = 'u645697248_ThrkF';
+    $username = 'u645697248_fKeCu';
+    $password = 'Ecoride123';
 
     try {
         $db = new PDO("mysql:host=$host;port=8889;dbname=$dbname;charset=utf8", $username, $password);
